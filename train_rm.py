f764c838c64fcfd69b2b1d9f8e5e424d6ba1f509
Artyom Stepanov <stepartm@gmail.com>
2026-04-13 19:55:38 +0300

    Class-based RM training

--- train_rm.py
+++ train_rm.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
+from typing import Optional
 
-import pandas as pd
 import torch
 from datasets import load_from_disk
 from torch.nn.utils import get_total_norm, clip_grad_norm_
@@ -10,114 +10,205 @@ from transformers import GPT2Tokenizer, GPT2ForSequenceClassification
 from tqdm import tqdm
 
 
-logsigmoid = torch.nn.LogSigmoid()
+def collate_wrapper(tokenizer, device):
+    def collate_(batch):
+        winner_inputs = tokenizer(
+            [_['winner'] for _ in batch], padding=True, truncation=True,
+            return_tensors="pt", add_special_tokens=True, padding_side="left",
+        )
 
-def compute_loss(model, batch, do_not_need_grad=False):
-    sample_winner_inputs, sample_loser_inputs = batch
+        for k, v in winner_inputs.items():
+            winner_inputs[k] = v.to(device)
 
-    with torch.autograd.grad_mode.inference_mode(mode=do_not_need_grad):
-        winner_logits  = model(**sample_winner_inputs).logits[:, 1]
-        loser_logits  = model(**sample_loser_inputs).logits[:, 1]
+        loser_inputs = tokenizer(
+            [_['loser'] for _ in batch], padding=True, truncation=True,
+            return_tensors="pt", add_special_tokens=True, padding_side="left",
+        )
 
-    return -logsigmoid(winner_logits - loser_logits).mean()
+        for k, v in loser_inputs.items():
+            loser_inputs[k] = v.to(device)
 
+        return winner_inputs, loser_inputs
 
-def collate_fn(batch):
-    winner_inputs = tokenizer(
-        [_['winner'] for _ in batch], padding=True, truncation=True,
-        return_tensors="pt", add_special_tokens=True, padding_side="left",
-    )
-
-    for k, v in winner_inputs.items():
-        winner_inputs[k] = v.to(DEVICE)
-
-    loser_inputs = tokenizer(
-        [_['loser'] for _ in batch], padding=True, truncation=True,
-        return_tensors="pt", add_special_tokens=True, padding_side="left",
-    )
-
-    for k, v in loser_inputs.items():
-        loser_inputs[k] = v.to(DEVICE)
-
-    return winner_inputs, loser_inputs
+    return collate_
 
 
-if __name__ == "__main__":
-    DEVICE = torch.device("cuda:0")
-    BATCH_SIZE = 48
-    NUM_WORKERS = 0
-    EVAL_EVERY = 1
-    LR = 3e-6
-    WD = 0.01
-    CLIP_GRAD_VAL = float("inf")
-    # CLIP_GRAD_VAL = 15
+class Trainer:
 
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    tokenizer.add_special_tokens({"pad_token": "<|endoftext|>"})
-    tokenizer.add_eos_token = True
+    def __init__(
+            self,
+            rank: int,
+            n_iterations: Optional[int],
+            lr: float,
+            weight_decay: float,
+            scheduler_hparams: dict,
+            dataloader_hparams: dict,
+            clip_grad_val: float=float('inf'),
+            max_norm: float=10,
+            eval_every: int = 1
+        ):
 
-    SFT_CHECKPOINT_PATH = '/home/logsumexp/workspace/rl-summarizer/artifacts/sft/Mar31_07-34-36_lr=3e-05_weight_decay=0_clip_grad_val=inf'
-    reward_model = GPT2ForSequenceClassification.from_pretrained(SFT_CHECKPOINT_PATH)
-    reward_model.config.pad_token_id = tokenizer.pad_token_id
-    reward_model.to(DEVICE)
+        self.logsigmoid = torch.nn.LogSigmoid()
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        self.tokenizer.add_special_tokens({"pad_token": "<|endoftext|>"})
+        self.tokenizer.add_eos_token = True
 
-    comparison_dataset_train = load_from_disk("datasets/comparison/train_preprocessed")
-    comparison_dataset_val = load_from_disk("datasets/comparison/val_preprocessed")
+        self.n_iterations = n_iterations
+        self.rank = rank
+        self.reward_model = GPT2ForSequenceClassification.from_pretrained('/home/logsumexp/workspace/rl-summarizer/artifacts/sft/Mar31_07-34-36_lr=3e-05_weight_decay=0_clip_grad_val=inf')
+        self.reward_model.config.pad_token_id = self.tokenizer.pad_token_id
+        self.reward_model.to(self.rank)
 
-    rm_train_dl = torch.utils.data.DataLoader(comparison_dataset_train, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, collate_fn=collate_fn)
-    rm_val_dl = torch.utils.data.DataLoader(comparison_dataset_val, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS, collate_fn=collate_fn)
+        self.optimizer = torch.optim.Adam(
+            self.reward_model.parameters(),
+            lr=lr,
+            weight_decay=weight_decay
+        )
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, **scheduler_hparams)
 
-    optimizer = torch.optim.Adam(reward_model.parameters(), lr=LR, weight_decay=WD)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2], gamma=0.1)
+        comparison_dataset_train = load_from_disk("datasets/comparison/train_preprocessed")
+        comparison_dataset_val = load_from_disk("datasets/comparison/val_preprocessed")
 
-    iteration_number = 0
-    training_step = 0
-    current_time = datetime.now().strftime("%b%d_%H-%M-%S")
-    model_name = f"rm/{current_time}_lr={LR}_weight_decay={WD}_clip_grad_val={CLIP_GRAD_VAL}"
-    writer = SummaryWriter(log_dir=os.path.join("logs", model_name))
+        collate_fn = collate_wrapper(tokenizer=self.tokenizer, device=self.rank)
 
-    # while True:
-    for _ in range(20):
-        reward_model.train()
-        for train_batch in tqdm(rm_train_dl, desc=f"Training iteration {iteration_number}"):
-            training_step += 1
-            training_loss = compute_loss(model=reward_model, batch=train_batch)
-            writer.add_scalar(tag="loss/train", scalar_value=training_loss, global_step=training_step)
+        self.rm_train_dl = torch.utils.data.DataLoader(
+            comparison_dataset_train, collate_fn=collate_fn,
+            **dataloader_hparams
+        )
+        self.rm_val_dl = torch.utils.data.DataLoader(
+            comparison_dataset_val, collate_fn=collate_fn,
+            **dataloader_hparams
+        )
+
+        self.training_step = 0
+
+        current_time = datetime.now().strftime("%b%d_%H-%M-%S")
+        self.model_name = f"rm/{current_time}_lr={lr}_weight_decay={weight_decay}_clip_grad_val={clip_grad_val}"
+        self.writer = SummaryWriter(log_dir=os.path.join("logs", self.model_name))
+
+        self.clip_grad_val = clip_grad_val
+        self.max_norm = max_norm
+        self.eval_every = eval_every
+
+    def compute_loss(self, batch, do_not_need_grad=False):
+        sample_winner_inputs, sample_loser_inputs = batch
+
+        with torch.autograd.grad_mode.inference_mode(mode=do_not_need_grad):
+            winner_logits = self.reward_model(**sample_winner_inputs).logits[:, 1]
+            loser_logits  = self.reward_model(**sample_loser_inputs).logits[:, 1]
+
+        return -self.logsigmoid(winner_logits - loser_logits).mean()
+
+
+    def train_iteration(self, iteration_number: int):
+        self.reward_model.train()
+        for train_batch in tqdm(self.rm_train_dl, desc=f"Training iteration {iteration_number}"):
+            self.training_step += 1
+            training_loss = self.compute_loss(batch=train_batch)
+            self.writer.add_scalar(tag="loss/train", scalar_value=training_loss, global_step=self.training_step)
 
             training_loss.backward()
 
-            if CLIP_GRAD_VAL < float('inf'):
-                grad_norm_before_clipping = clip_grad_norm_(parameters=reward_model.parameters(), error_if_nonfinite=True, max_norm=10)
-                writer.add_scalar(tag="grad_norm/before_clipping", scalar_value=grad_norm_before_clipping, global_step=training_step)
+            if self.clip_grad_val < float('inf'):
+                grad_norm_before_clipping = clip_grad_norm_(
+                    parameters=self.reward_model.parameters(),
+                    error_if_nonfinite=True, max_norm=self.max_norm
+                )
+                self.writer.add_scalar(
+                    tag="grad_norm/before_clipping",
+                    scalar_value=grad_norm_before_clipping,
+                    global_step=self.training_step
+                )
 
             grad_norm_after_clipping = get_total_norm(
-                tensors=(p.grad for p in reward_model.parameters() if p.grad is not None), error_if_nonfinite=True
+                tensors=(p.grad for p in self.reward_model.parameters() if p.grad is not None),
+                error_if_nonfinite=True
             )
-            writer.add_scalar(tag="grad_norm/after_clipping", scalar_value=grad_norm_after_clipping, global_step=training_step)
+            self.writer.add_scalar(
+                tag="grad_norm/after_clipping",
+                scalar_value=grad_norm_after_clipping,
+                global_step=self.training_step
+            )
 
-            optimizer.step()
-            optimizer.zero_grad()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
 
-        if iteration_number % EVAL_EVERY == 0:
-            reward_model.eval()
+        if iteration_number % self.eval_every == 0:
+            self.reward_model.eval()
             validation_loss = 0
             num_validation_batches = 0
-            for val_batch in tqdm(rm_val_dl):
-                validation_loss += compute_loss(model=reward_model, batch=val_batch, do_not_need_grad=True)
+            for val_batch in tqdm(self.rm_val_dl):
+                validation_loss += self.compute_loss(batch=val_batch, do_not_need_grad=True)
                 num_validation_batches += 1
 
-            writer.add_scalar(
+            self.writer.add_scalar(
                 tag="loss/val",
                 scalar_value=validation_loss / num_validation_batches,
                 global_step=iteration_number
             )
 
-        writer.add_scalar(
+        self.writer.add_scalar(
             tag="lr",
-            scalar_value=scheduler.get_last_lr()[0],
+            scalar_value=self.scheduler.get_last_lr()[0],
             global_step=iteration_number
         )
-        scheduler.step()
-        iteration_number += 1
+        self.scheduler.step()
 
-    reward_model.save_pretrained(os.path.join("artifacts", model_name))
+    def train(self):
+        self.training_step = 0
+        if isinstance(self.n_iterations, int):
+            for iteration_number in range(self.n_iterations):
+                self.train_iteration(iteration_number=iteration_number)
+        else:
+            iteration_num = 0
+            while True:
+                self.train_iteration(iteration_number=iteration_number)
+                iteration_num += 1
+
+        self.reward_model.save_pretrained(
+            os.path.join("artifacts", self.model_name)
+        )
+
+def main(
+        rank: int, n_iterations: int | None, lr: float, weight_decay: float, milestones: list, gamma: float, batch_size: int,
+        num_workers: int, clip_grad_val: float, max_norm: float, eval_every: int
+    ):
+    trainer = Trainer(
+        rank=rank,
+        n_iterations=n_iterations,
+        lr=lr,
+        weight_decay=weight_decay,
+        scheduler_hparams={
+            "milestones": milestones,
+            "gamma": gamma
+        },
+        dataloader_hparams={
+            "batch_size": batch_size,
+            "shuffle": True,
+            "num_workers": num_workers
+        },
+        clip_grad_val=clip_grad_val,
+        max_norm=max_norm,
+        eval_every=eval_every
+    )
+    trainer.train()
+
+if __name__ == "__main__":
+
+    rank = 1
+    n_iterations = 20
+    lr = 3e-6
+    weight_decay = 0.01
+    milestones=[2]
+    gamma=0.1
+    batch_size = 48
+    num_workers = 0
+    clip_grad_val = float("inf")
+    max_norm = 10
+    eval_every = 1
+
+    main(
+        rank=rank, n_iterations=n_iterations, lr=lr, weight_decay=weight_decay,
+        milestones=milestones, gamma=gamma, batch_size=batch_size, num_workers=num_workers, clip_grad_val=clip_grad_val,
+        max_norm=max_norm, eval_every=eval_every
+    )
